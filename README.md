87c301b110e295f218465c3d05da5c09a128347f
K W <fake2823email29@email.com>
2021-10-02 23:57:53 -0400

    update routes

--- README.md
+++ README.md
@@ -7,12 +7,49 @@ Add info about the project here
 ### Run locally
 1. Clone this repository
 2. Create .env and add `MONGO_URI=<MongoDB URI>`
-3. Run `npm run i` and `npm run start`
+3. Run `npm i;npm run init` to initialize database
+4. Restart API with `npm run start`
 
 ### Deploying to heroku
 1. Clone this repository
 2. Add `MONGO_URI` as an environment variable
-3. `npm run deploy`
+3. Run `npm run deploy` to start API
+
+### API Routes
+`/publishers` - List of data set publishers<br>
+`/search` - Search database for term
+```
+QUERY PARAMETERS
+  (required) ?query=term - Regex search for term
+  (optional) ?property=title - Search by property name specified in data.gov metadata schema https://resources.data.gov/resources/dcat-us/
+  (optional) ?limit=100 - Number of results returned
+  (optional) ?offset=0 - Pagination tool for search results, offset number of entries
+
+RETURN
+  {
+    data: [{
+      title: String,
+      description: String,
+      theme: [String],
+      keyword: [String],
+      modified: Date,
+      bureauCode: [String],
+      programCode: [String],
+      accessLevel: String,
+      landingPage: String,
+      issued: Date,
+      publisher: String,
+    }]
+    query: String,
+    property: String,
+    offset: Number,
+    limit: Number,
+    total: Number // Number of total entries
+  }
+
+EXAMPLE
+https://nasadata.herokuapp.com/search?query=apollo%20program&property=description&limit=3
+```
 
 ## Frontend website
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,40 +3,39 @@ var router = express.Router();
 import { Dataset } from '../models/list.js';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Hello world!' });
 });
 
 /* Search by property */
-router.get('/search', function(req, res, next) {
-  var queries = req.query;
-  var properties = Object.keys(queries);
+router.get('/search', function (req, res, next) {
+  var query = req.query.query;
+  var property = req.query.property || "title";
   var limit = req.query.limit || 100;
   var offset = req.query.offset || 0;
 
   // Find in text
   var queryObj = {};
-  for (properties of properties) {
-    queryObj[property] = { $regex: query, $options: 'i' };
-  }
+  queryObj[property] = { $regex: query, $options: 'i' };
 
   console.log(query, property, limit, offset, queryObj);
-  
 
-  Dataset.find(queryObj).limit(limit).skip(offset).exec(function(err, data) {
+
+  Dataset.find(queryObj).limit(limit).skip(offset).exec(function (err, data) {
     if (err) {
       res.status(500).send(err);
     } else {
-      // Get total count
-      Dataset.count(queryObj, function(err, count) {
+      // Print number of datasets
+      Dataset.count(queryObj, function (err, count) {
         if (err) {
           res.status(500).send(err);
         } else {
-          // Remove _id and __v
-          data.forEach(function(item) {
-            delete item._id;
-            delete item.__v;
-          });
+          // Delete _id and __v from data
+          for (var i = 0; i < data.length; i++) {
+            delete data[i]._id;
+            delete data[i].__v;
+          }
+
           res.json({
             data: data,
             total: count,
@@ -46,29 +45,12 @@ router.get('/search', function(req, res, next) {
             offset: offset
           });
         }
-      });
+      })
     }
   });
+
 
 })
-
-// Get set of all properties (e.g. publishers)
-router.get('/publishers', function(req, res, next) {
-  let limit = req.query.limit || 100;
-  let offset = req.query.offset || 0;
-
-  Dataset.distinct("publisher", function(err, data) {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.json(data);
-    }
-  });
-});
-
-// Get all themes (themes is a list of strings)
-
-
 
 
 export default router;
